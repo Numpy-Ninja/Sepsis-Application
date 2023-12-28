3ddeb93f0f8dcddbccada6fa159874854863d84c
kalairvk <110203820+kalairvk@users.noreply.github.com>
2023-12-28 11:13:50 -0600

    formatting changes

--- src/test/java/stepDefinitions/UploadFileBloodReportSD.java
+++ src/test/java/stepDefinitions/UploadFileBloodReportSD.java
@@ -11,27 +11,9 @@ public class UploadFileBloodReportSD {
 	
 	UploadFileBloodReport bloodReport= new UploadFileBloodReport();
 	String actualMsg;
-	String actualSepsisStatus;
-	String expectedSepsisStatus;
-	
-	
-	@Given("Patient is on details tab in saved patient form")
-	public void patient_is_on_details_tab_in_saved_patient_form() {
-		
-	}
-
-	
-	@Then("the status of RiskOfSepsis should be shown {string}")
-	public void the_status_of_RiskOfSepsis_should_be_shown(String sepsisStatus ) {
-	   actualSepsisStatus=bloodReport.getSepsisRisk();
-	   expectedSepsisStatus=bloodReport.getRiskOfSepsis(sepsisStatus);
-	   assertEquals(actualSepsisStatus,expectedSepsisStatus);
-	}
 	
 	@Given("Patient is on related tab in saved patient form")
 	public void patient_is_on_related_tab_in_saved_patient_form() {
-	    
-
 	
 	    bloodReport.ClickRelatedTab();
 	}
@@ -41,8 +23,6 @@ public class UploadFileBloodReportSD {
 		bloodReport.enterFilePath();
 		
 	}
-
-
 
 	@Then("Patient should able to upload a file with a successful message {string}")
 	public void patient_should_able_to_upload_a_file_with_a_successful_message(String expectedMsg) throws InterruptedException {
--- src/test/resources/features/06UploadFileBloodReport.feature
+++ src/test/resources/features/06UploadFileBloodReport.feature
@@ -1,16 +1,7 @@
 @UploadBloodReport
 Feature: Validitate Blood Profile upload files
 
-  @TS_01ValidateRiskOfSepsis
-  Scenario Outline: Validitate Risk Of Sepsis
-    Given Patient is on details tab in saved patient form
-    Then the status of RiskOfSepsis should be shown "<SepsisStatus>"
-
-    Examples: 
-      | SepsisStatus |
-      | LOW      |
-
-  @TS_02ValiditateBloodProfileuploadfiles
+  @TS_01ValiditateBloodProfileuploadfiles
   Scenario: Validitate Blood Profile upload files
     Given Patient is on related tab in saved patient form
     When Patient clicks on the upload file option
